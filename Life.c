0f16f77af5c77d69cf0267f0288c83dc3bae660b
gracedamilola <56761746+gracedamilola@users.noreply.github.com>
2019-10-21 00:32:12 +0100

    Add files via upload

---
+++ Life.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+    {
+    float w= get_float ("weight (kg):");
+    float w= get_float ("height (m):");
+    
+    printf ("BMI=%f kg/m^2\",w/(h*h));
+   }
+
+            
+            
+        
