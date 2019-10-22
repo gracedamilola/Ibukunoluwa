93a5fef5ba2ada96d20138cd7f94ea8557b18427
gracedamilola <56761746+gracedamilola@users.noreply.github.com>
2019-10-22 12:28:32 +0100

    Add files via upload

---
+++ success.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    float w=get_float("weight(kg): ");
+    float h=get_float("height(m): ");
+    
+    
+    printf ("BMI=%f kg/m^2\n", w/(h*h));
+    
+}
